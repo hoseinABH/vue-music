844388144d0988e5377d1e0755d53104b9c357ca
Hosein Abolhasani <hosein.abolhasani1998@gmail.com>
2021-09-22 12:54:37 +0330

    add state for auth modal using vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
-import { createStore } from 'vuex';
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    authModalShow: false
   },
   mutations: {
+    toggleAuthModal: state => {
+      state.authModalShow = !state.authModalShow;
+    }
   },
-  actions: {
-  },
-  modules: {
-  },
+  actions: {},
+  modules: {}
 });
